6f265111ae750d0f0b209b9c31e2cdc35fbede09
jayashri23 <jsjankar23@gmail.com>
2022-10-20 01:21:31 +0530

    Welcome To Employee Wage Programs on Master Branch

---
+++ src/employeewage/welcome.java
@@ -0,0 +1,8 @@
+package employeewage;
+
+public class welcome {
+
+    public static void main(String [] args){
+        System.out.println("Welcome To Employee Wage Computation Program on Master Branch");
+    }
+}
