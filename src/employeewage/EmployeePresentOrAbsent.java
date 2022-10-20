1a22ee5b82f2d882ce5ed09b73598aab0fccb470
jayashri23 <jsjankar23@gmail.com>
2022-10-20 08:34:31 +0530

    Checking Employee Present Or Absent By Methods

--- src/employeewage/EmployeePresentOrAbsent.java
+++ src/employeewage/EmployeePresentOrAbsent.java
@@ -1,2 +1,21 @@
-package employeewage;public class EmployeePresentOrAbsent {
+package employeewage;
+
+
+import java.util.Random;
+
+public class EmployeePresentOrAbsent {
+    public void check() {
+
+        Random random = new Random();
+        int rand = random.nextInt(2);
+        if (rand == 1) {
+            System.out.println(" Employee is Present  ");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
+    public static void main(String[] args) {
+        EmployeePresentOrAbsent obj =new EmployeePresentOrAbsent();
+        obj.check();
+    }
 }
