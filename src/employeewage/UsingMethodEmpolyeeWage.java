89379dad2a698668179022701e273bc0b9fc19c8
jayashri23 <jsjankar23@gmail.com>
2022-10-20 12:12:07 +0530

    Calculated Employee Wage Using Method

--- src/employeewage/UsingMethodEmpolyeeWage.java
+++ src/employeewage/UsingMethodEmpolyeeWage.java
@@ -1,2 +1,16 @@
-package employeewage;public class UsingMethodEmpolyeeWage {
+package employeewage;
+
+public class UsingMethodEmpolyeeWage {
+
+    static int EmployeeWage(int wage ,int workingHr ){
+
+            int salary = wage * workingHr;
+            return salary;
+    }
+    public static void main (String [] args){
+     int emp1= EmployeeWage(20,8);
+     int emp2= EmployeeWage(30 , 16);
+        System.out.println("Employee Wage = " +emp1);
+        System.out.println("Employee Wage = " +emp2);
+    }
 }
